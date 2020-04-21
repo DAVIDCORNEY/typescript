f212ddcba4bbdd8d2572d7b4715b6d3605b83ef8
David Corney <david.corney@coveainsurance.co.uk>
2020-04-21 10:58:23 +0100

    Add types to function parameters

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Code goes here!');
+function add(n1, n2, showResult, returnMessage) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(returnMessage + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 3;
+var printResult = true;
+var message = 'Result is ';
+add(number1, number2, printResult, message);
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log('Code goes here');
+function add(n1: number,n2: number, showResult: boolean, returnMessage: string) {
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(returnMessage + result);
+    } else {
+        return result;
+    }
+}
+
+const number1 = 5;
+const number2 = 3;
+const printResult = true;
+const message = 'Result is ';
+
+add(number1, number2, printResult, message);
+
